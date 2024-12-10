167dde423e02940e6a92a44ec22967735cde27a5
NathanHarden <wenweisen167@gmail.com>
2024-12-10 23:30:12 +0800

    第五次提交

--- day2-client/main/main.go
+++ day2-client/main/main.go
@@ -1,12 +1,11 @@
 package main
 
 import(
-	"encoding/json"
 	"fmt"
-	"gorpc/day2-client"
-	"gorpc/day2-client/codec"
+	"gorpc"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -17,27 +16,6 @@ func startServer(addr chan string){
 	}
 	log.Println("start rpc server on",l.Addr())
 	addr<-l.Addr().String()
-	gorpc.Accept(l)
+	gorpc.Accept()
 }
 
-func main(){
-	log.SetFlags(0)
-	addr:=make(chan string)
-	go startServer(addr)
-	conn,_:=net.Dial("tcp",<-addr)
-	defer func(){_=conn.Close()}()
-	time.Sleep(time.Second)
-	_=json.NewEncoder(conn).Encode(gorpc.DefaultOption)
-	cc:=codec.NewGobCodec(conn)
-	for i:=0;i<5;i++{
-		h:=&codec.Header{
-			ServiveMethod: "Foo.Sum",
-			Seq:uint64(i),
-		}
-		_=cc.Write(h,fmt.Sprintf("gorpc req %d",h.Seq))
-		_=cc.ReadHeader(h)
-		var reply string
-		_=cc.ReadBody(&reply)
-		log.Println("reply: ",reply)
-	}
-}
